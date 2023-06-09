b68cc25fae1e06a2bf36dce5c25c92302333058e
steeper <portland.steeper@gmail.com>
2023-06-09 15:15:20 -0400

    making some progress in the re setup of Tailwind. i got rid of home.modules

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className="flex">
       <div className="w-1/5 bg-white">
-        
+
       </div>
       <div className="w-4/5 bg-white">
         <div>Hoe</div>
